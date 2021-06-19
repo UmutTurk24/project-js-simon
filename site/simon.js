6f637cec612b5e3c751a95ac6c3cce9310976fc9
UmutTurk24 <78030374+UmutTurk24@users.noreply.github.com>
2021-06-20 01:31:32 +0300

    interactions with buttons

--- site/simon.js
+++ site/simon.js
@@ -1,69 +1,149 @@
+let greenButton = document.querySelector('.simon-button.green');
+let redButton = document.querySelector('.simon-button.red');
+let yellowButton = document.querySelector('.simon-button.yellow');
+let blueButton = document.querySelector('.simon-button.blue');
+let nextRoundBox = document.querySelector('#nextRoundBox');
+let body = document.querySelector('body');
+var currentRound = 0;
+
+function disableButtons(){
+  greenButton.disabled = true;
+  redButton.disabled = true;
+  yellowButton.disabled = true;
+  blueButton.disabled = true;
+}
+
+function enableButtons(){
+  greenButton.disabled = false;
+  redButton.disabled = false;
+  yellowButton.disabled = false;
+  blueButton.disabled = false;
+}
+
+
 function newColor(){
   let newNum = Math.floor(Math.random() * 4) + 1;
-  return newNum;
+  let nextColor;
+  if(newNum = 1){
+    nextColor = "green";
+  }
+  else if(newNum = 2){
+    nextColor = "red";
+  }
+  else if(newNum = 3){
+    nextColor = "yellow";
+  }
+  else{
+    nextColor = "blue";
+  }
+  return nextColor;
 }
 
-function nextRound(){
+function nextRound(colorSequence){
+  disableButtons();
+  currentRound++;
+  nextRoundBox.innerText = "Round: " + currentRound;
+  nextRoundBox.style.visibility = "visible";
+  setTimeout(function(){
+    nextRoundBox.style.visibility = "hidden";
+  },4000);  
+  colorSequence.push(newColor());
+  playSequence(colorSequence);
+  enableButtons();
+}
+
+function playSequence(colorSequences){
+  setTimeout(function(){
+    for (let x = 0; x < colorSequences.length; x++ ){
+      let color = colorSequences[x];
+      setTimeout(function(){
+        let curButton = document.querySelector('.' + color);
+        let revertedStyle = curButton.style;
+        playSound(color,time,player,revertedStyle);
+      },1000);  
+    }
+  }, 6500);
 
 }
 
-function playSequence(){
+function startGame(){
+  
+  if (currentRound == 0)
+  {
+    let sequence = [];
+    let titleGone = document.querySelector('h1');
+    titleGone.innerHTML="";
+    nextRound(sequence);
+  }
+  nextRound(sequence);
 
 }
 
-function playGame(){
-  let sequence = [];
 
-}
+var sound;
+var time = 500;
+var player = true;
 
+function playSound(buttonName, time, player, buttonStyle) {
+  let curButton = document.querySelector('.' + buttonName);
+  sound = new Audio("sound-" + buttonName + ".m4a");
+  sound.play();
 
+  if(player){
+    setTimeout(function(){
+      sound.pause();
+      sound.currentTime = 0;
+      curButton.style = buttonStyle; 
+    },time);  
+  }else{
 
-
-
-let greenButton = document.querySelector('.simon-button.green');
-greenButton.addEventListener('click', function() {
-  alert('You clicked the green button');
-});
-
-let redButton = document.querySelector('.simon-button.red');
-redButton.addEventListener('click', function() {
-  alert('You clicked the red button');
-});
-
-let yellowButton = document.querySelector('.simon-button.yellow');
-yellowButton.addEventListener('click', function() {
-  alert('You clicked the yellow button');
-});
-
-let blueButton = document.querySelector('.simon-button.blue');
-blueButton.addEventListener('click', function() {
-  alert('You clicked the blue button');
-});
-
-
-function PlaySound() {
-  alert("hello");
-  var sound = new audio();
-  bflat.src = "bflat.mp3";
-  document.getElementById(bflat);
-  bflat.Play();
+  }
+  
 }
 
 document.addEventListener('keydown', function(event) {
   if(event.code == "ArrowDown"){
-    document.querySelector(`#colorText`).style.color = "brown";
+    let sequence = [];
+    nextRound(sequence);
+    let originalStyle = redButton.style;
+    redButton.style.backgroundColor = "#ff5e5e";
+    playSound("red", time, player, originalStyle);
   }
   if(event.code == "ArrowUp"){
-    document.querySelector(`#colorText`).style.color = "brown";
+    let originalStyle = greenButton.style;
+    greenButton.style.backgroundColor = "#55ff7c";
+    playSound("green", time, player);
   }
   if(event.code == "ArrowLeft"){
-    document.querySelector(`#colorText`).style.color = "brown";
+    let originalStyle = blueButton.style;
+    blueButton.style.backgroundColor = "#57b6ff";
+    playSound("blue", time, player);
   }
   if(event.code == "ArrowRight"){
-    document.querySelector(`#colorText`).style.color = "brown";
+    let originalStyle = yellowButton.style;
+    yellowButton.style.backgroundColor = "#ffd557";
+    playSound("yellow", time, player);
   }
-
 });
+
+/* document.addEventListener('onkeyup', function(event) {
+  if(event.code == "ArrowDown"){
+    redButton.style.backgroundColor = "#ff0505";
+    sound.pause();
+  }
+  if(event.code == "ArrowUp"){
+    greenButton.style.backgroundColor = "#ff0505";
+    playSound("green");
+  }
+  if(event.code == "ArrowLeft"){
+    blueButton.style.backgroundColor = "#ff0505";
+    playSound("green");
+  }
+  if(event.code == "ArrowRight"){
+    yellowButton.style.backgroundColor = "#ff0505";
+    playSound("green");
+  }
+}); */
 
 
 
@@ -85,4 +165,5 @@ function startTimer(){
   {
       document.getElementById("count").style="color:green;";
       startTimer();
+      startGame();
   };
